d43daa86a71a275c89533269e80ecfaea7fb3c65
BrkIt <BrkIt@users.noreply.github.com>
2020-08-06 15:40:41 +1000

    Update KongPincrementalNoScroll.user.js

--- KongPincrementalNoScroll.user.js
+++ KongPincrementalNoScroll.user.js
@@ -4,6 +4,7 @@
 // @version      0.1
 // @author       NDakota
 // @match        http*://www.kongregate.com/games/makopaz/pincremental*
+// @match        http*://www.kongregate.com/games/Makopaz/pincremental*
 // @grant        none
 // ==/UserScript==
 
