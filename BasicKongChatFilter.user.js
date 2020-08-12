b19e4a6c468f3d9595252edc2ea0abc8ffcdb984
BrkIt <BrkIt@users.noreply.github.com>
2020-08-12 12:17:55 +1000

    Update BasicKongChatFilter.user.js

--- BasicKongChatFilter.user.js
+++ BasicKongChatFilter.user.js
@@ -4,7 +4,7 @@
 // @match       https://www.kongregate.com/games/makopaz/pincremental
 // @match       https://www.kongregate.com/games/Makopaz/pincremental
 // @grant       none
-// @version     1.2.1
+// @version     1.3
 // @downloadURL https://github.com/BrkIt/BrkIt.github.io/raw/master/BasicKongChatFilter.user.js
 // @updateURL   https://github.com/BrkIt/BrkIt.github.io/raw/master/BasicKongChatFilter.user.js
 // @author      Alexiea
@@ -13,8 +13,6 @@
 
 // *********** This script will stop working as soon as the bots update their message. DM me on discord and I'll update it. @Alexiea#6630 ***********
 
-console.log('[Basic KongChat Filter] Initializing...');
-            
 function KongFilter() {
   var els = document.getElementsByClassName("chat-message");
   var searchValue = /yo.*play.*nude.*site.*http/;                 //This is the message that we're looking to remove.
@@ -33,20 +31,26 @@ function KongFilter() {
   }
 }
 
-setInterval(KongFilter, 1000);
+function initialise() {
 
+  console.log('[Basic KongChat Filter] Initializing...');
 
-//Load spamRemoved count
-spamRemoved = 0;
-if (localStorage.getItem("BKCFspamRemoved")) {
-  spamRemoved = localStorage.getItem("BKCFspamRemoved");
+  //Load spamRemoved count
+  spamRemoved = 0;
+  if (localStorage.getItem("BKCFspamRemoved")) {
+    spamRemoved = localStorage.getItem("BKCFspamRemoved");
+  }
+
+  //Create <span> to display spamRemoved Count
+  var span = document.createElement("SPAN");
+  span.style.float = "right";
+  span.innerHTML = "Spam Removed:" + spamRemoved;
+  document.getElementById("chat_window_header").children[0].appendChild(span);
+  
+  setInterval(KongFilter, 1000);
+
+  console.log('[Basic KongChat Filter] Loaded!');
+  console.log('[Basic KongChat Filter] Total Removed: ' + spamRemoved);
 }
 
-//Create <span> to display spamRemoved Count
-var span = document.createElement("SPAN");
-span.style.float = "right";
-span.innerHTML = "Spam Removed:" + spamRemoved;
-document.getElementById("chat_window_header").children[0].appendChild(span); 
-
-console.log('[Basic KongChat Filter] Loaded!');
-console.log('[Basic KongChat Filter] Total Removed: ' + spamRemoved);
+setTimeout(initialise, 10000);
