73ba4afe6157fd31352add78181de8a49f2d87a6
BrkIt <BrkIt@users.noreply.github.com>
2020-08-22 03:55:47 +1000

    Update KongChatroomSwitcher.user.js

--- KongChatroomSwitcher.user.js
+++ KongChatroomSwitcher.user.js
@@ -3,7 +3,7 @@
 // @namespace   Alexiea
 // @match       https://www.kongregate.com/games/*
 // @grant       none
-// @version     1.0.3
+// @version     1.0.4
 // @author      @Alexiea#6630
 // @downloadURL https://github.com/BrkIt/BrkIt.github.io/raw/master/KongChatroomSwitcher.user.js
 // @updateURL   https://github.com/BrkIt/BrkIt.github.io/raw/master/KongChatroomSwitcher.user.js
@@ -13,11 +13,20 @@
 function isOdd(num) { return num % 2;}
 
 function initialise() {
+
+  //Gets stored default chat
+  defaultChat = 321971;
+  if (localStorage.getItem("KCSDefault")) {
+    defaultChat = localStorage.getItem("KCSDefault");
+  }
  
-  var hook = document.getElementById('main_tab_set');
 
-  if (hook) { 
+  var hook = document.getElementById("chat_connected_indicator").innerHTML;
 
+  if (hook == "Connected") { 
+    
+    var hook = document.getElementById('main_tab_set');
+    //Add the tab
     var switcher = document.createElement("li");
     switcher.setAttribute("class", "tab");
     switcher.setAttribute("style","float: right; cursor: pointer;");
@@ -28,6 +37,7 @@ function initialise() {
     tab.innerHTML = "Chat Switcher";
     switcher.appendChild(tab);
 
+    //Add ul for chats
     var ul = document.createElement("ul");
     ul.setAttribute("id", "chat_switcher");
     ul.setAttribute("style",`
@@ -46,63 +56,92 @@ function initialise() {
     `);
     switcher.appendChild(ul);
 
+    //CSS for <li>
     var style = document.createElement('style');
     style.innerHTML = 
       `#chat_switcher li {cursor: pointer;display:block;text-align:left;color:#900;text-decoration: underline;padding: 1px 0;}
       #chat_switcher li:nth-child(even) { background: #e3e3e3; }
-      #chat_switcher li:nth-child(odd) { background: white; }`;
+      #chat_switcher li:nth-child(odd) { background: white; }
+      #chat_switcher input { margin-right: 3px }`;
 
     var ref = document.querySelector('script');
     ref.parentNode.insertBefore(style, ref);
 
+    //The list of games
     var game_list = {
-      1: { title:"Animation Throwdown", game:"animation-", id:"271381" },
-      2: { title:"BattleHand", game:"battlehand", id:"261051" },
+      1: { title:"Animation Throwdown", game:"animation-throwdown", id:"271381" },
+      2: { title:"BattleHand", game:"battlehand-web", id:"261051" },
       3: { title:"Bit Heroes", game:"bit-heroes", id:"266462" },
-      4: { title:"Clicker Heroes", game:"clicker-he", id:"216826" },
-      5: { title:"Cosmos Quest: The Origin", game:"cosmos-que", id:"276144" },
-      6: { title:"Crusaders of the Lost Idols", game:"crusaders-", id:"243503" },
-      7: { title:"Crush Crush", game:"crush-crus", id:"256923" },
+      4: { title:"Clicker Heroes", game:"clicker-heroes", id:"216826" },
+      5: { title:"Cosmos Quest: The Origin", game:"cosmos-quest", id:"276144" },
+      6: { title:"Crusaders of the Lost Idols", game:"crusaders-of-the-lost-idols", id:"243503" },
+      7: { title:"Crush Crush", game:"crush-crush", id:"256923" },
       8: { title:"DPS IDLE", game:"dps-idle", id:"316416" },
-      9: { title:"Dungeon Crusher: Soul Hunters", game:"dungeon-cr", id:"283352" },
+      9: { title:"Dungeon Crusher: Soul Hunters", game:"dungeon-crusher-soul-hunters", id:"283352" },
       10: { title:"Firestone Idle RPG", game:"firestone", id:"299519" },
-      11: { title:"Idle Champions of the Forgotten Realms", game:"idle-champ", id:"303551" },
-      12: { title:"Idle Grindia: Dungeon Quest", game:"idle-grind", id:"317742" },
-      13: { title:"Idle Monster TD", game:"idle-monst", id:"315606" },
+      11: { title:"Idle Champions of the Forgotten Realms", game:"idle-champions-of-the-forgotten-realms", id:"303551" },
+      12: { title:"Idle Grindia: Dungeon Quest", game:"idle-grindia-dungeon-quest", id:"317742" },
+      13: { title:"Idle Monster TD", game:"idle-monster-td", id:"315606" },
       14: { title:"Idling to Rule the Gods", game:"idling-to-become-god", id:"218178" },
-      15: { title:"Incremental Epic Hero", game:"ncrementa", id:"317607" },
+      15: { title:"Incremental Epic Hero", game:"incremental-epic-hero", id:"317607" },
       16: { title:"Iron Rage", game:"iron-rage", id:"296562" },
-      17: { title:"Mighty Party", game:"mighty-par", id:"272366" },
-      18: { title:"Mob Wars: La Cosa Nostra", game:"mob-wars-l", id:"161320" },
-      19: { title:"Mutilate-a-Doll 2", game:"mutilate-a", id:"190329" },
-      20: { title:"Naruto Online", game:"naruto-onl", id:"269125" },
+      17: { title:"Mighty Party", game:"mighty-party", id:"272366" },
+      18: { title:"Mob Wars: La Cosa Nostra", game:"mob-wars-la-cosa-nostra", id:"161320" },
+      19: { title:"Mutilate-a-Doll 2", game:"mutilate-a-doll-2", id:"190329" },
+      20: { title:"Naruto Online", game:"naruto-online", id:"269125" },
       21: { title:"NGU IDLE", game:"ngu-idle", id:"287709" },
       22: { title:"Pincremental", game:"pincremental", id:"321971" },
-      23: { title:"Realm Grinder", game:"realm-grin", id:"239558" },
-      24: { title:"Realm of the Mad God", game:"realm-of-t", id:"127641" },
+      23: { title:"Realm Grinder", game:"realm-grinder", id:"239558" },
+      24: { title:"Realm of the Mad God", game:"realm-of-the-mad-god", id:"127641" },
       25: { title:"Spellstone", game:"spellstone", id:"248326" },
-      26: { title:"Supermechs", game:"supermechs", id:"171560" },
+      26: { title:"Supermechs", game:"supermechs", id:"171560" },								
       27: { title:"Synergism", game:"synergism", id:"320578" },
-      28: { title:"The Perfect Tower", game:"the-perfec", id:"258553" },
-      29: { title:"Tyrant Unleashed", game:"tyrant-unl", id:"208033" },
+      28: { title:"The Perfect Tower", game:"the-perfect-tower", id:"258553" },
+      29: { title:"Tyrant Unleashed", game:"tyrant-unleashed-web", id:"208033" },
       30: { title:"Wartune", game:"wartune", id:"303599" }
     };
 
+    //Generate the list
     var x;
     for (x in game_list) {
       var li = document.createElement("li");
-      li.innerHTML = game_list[x].title;
       li.setAttribute("title",game_list[x].title);
       li.setAttribute("game",game_list[x].game);
       li.setAttribute("id",game_list[x].id);
 	    li.onclick = function(li) { holodeck.selectRoom({"name": this.getAttribute("title") + "- Room #01","xmpp_name": this.getAttribute("id") + "-" + this.getAttribute("game") + "-1","type":"game"}) };
 	    ul.appendChild(li);
+      
+      //Add radio buttons to select default
+      var input = document.createElement("input");
+      input.setAttribute("type", "radio");
+      input.setAttribute("title","Default Chat Room");
+      input.setAttribute("name","Default");
+      input.onclick = function() { alert('hello'); };
+      li.appendChild(input);
+      
+      li.innerHTML = li.innerHTML + game_list[x].title;
     }
+    
+    //Checks default radio button
+    document.getElementById(defaultChat).children[0].checked = true;
+    
+    //Saves default chat
+    var x = document.querySelectorAll('[name="Default"]')
+    for(var i=0; i<x.length; i++) {
+      x[i].onclick = function (e) {
+        e.stopPropagation();
+        localStorage.setItem("KCSDefault", this.parentNode.id);
+      };
+    }
+    
+    //Join default chat
+    if (document.querySelector("#chat_default_content > p").innerHTML == "No Chat Rooms found!") { document.getElementById(defaultChat).click(); document.getElementById("chat_switcher").style.display = "none"; }
+
     console.log('[Kong Chatroom Switcher] Links Added!');
     clearInterval(initialise);
   }
 }
 
-var initialise = setInterval(initialise, 1000);
+var initialise = setInterval(initialise, 5000);
 
-//console.log('{ title:"' + document.querySelectorAll('[itemprop="name"]')[0].innerHTML + '"", game:""' + kong_ads._targetingParams.game + '", id:"' + kong_ads._targetingParams.game_id + '" },')
+//console.log('{ title:"' + document.querySelectorAll('[itemprop="name"]')[0].innerHTML + '", game:"' + active_user._game_attributes._object.game_permalink + '", id:"' + kong_ads._targetingParams.game_id + '" },')
