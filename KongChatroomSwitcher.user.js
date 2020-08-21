2044caa5426f8ec77a2dd705a52f97aa9c53013e
BrkIt <BrkIt@users.noreply.github.com>
2020-08-22 00:32:15 +1000

    Update KongChatroomSwitcher.user.js
    Added 23 more chats.
    Added some nice CSS.

--- KongChatroomSwitcher.user.js
+++ KongChatroomSwitcher.user.js
@@ -10,6 +10,7 @@
 // @description Adds a list of games that still have chatrooms on Kong and lets you switch between them.
 // ==/UserScript==
 
+function isOdd(num) { return num % 2;}
 
 function initialise() {
  
@@ -30,13 +31,13 @@ function initialise() {
     var ul = document.createElement("ul");
     ul.setAttribute("id", "chat_switcher");
     ul.setAttribute("style",`
-	    background-color: #fff;
+	    background-color: #e3e3e3;
 	    border: solid 1px #666;
 	    border-bottom-left-radius: 3px;
 	    border-bottom-right-radius: 3px;
 	    box-shadow: 0 1px 4px rgba(0,0,0,0.22);
 	    min-width: 150px;
-	    padding: 8px 0;
+	    padding: 8px;
 	    position: absolute;
       right: 2px;
       top: 52px;
@@ -45,26 +46,52 @@ function initialise() {
     `);
     switcher.appendChild(ul);
 
+    var style = document.createElement('style');
+    style.innerHTML = 
+      `#chat_switcher li {cursor: pointer;display:block;text-align:left;color:#900;text-decoration: underline;padding: 1px 0;}
+      #chat_switcher li:nth-child(even) { background: #e3e3e3; }
+      #chat_switcher li:nth-child(odd) { background: white; }`;
+
+    var ref = document.querySelector('script');
+    ref.parentNode.insertBefore(style, ref);
+
     var game_list = {
-      1: { title:"Pincremental", game:"pincremental", id:"321971" },
-      2: { title:"Idling to Rule the Gods", game:"idling-to-become-god", id:"218178" },
-      3: { title:"Spellstone", game:"spellstone", id:"248326" },
-      4: { title:"Incremental Epic Hero", game:"ncrementa", id:"317607" },
-      5: { title:"Synergism", game:"synergism", id:"320578" },
-      6: { title:"Firestone Idle RPG", game:"firestone", id:"299519" },
-      7: { title:"Realm of the Mad God", game:"realm-of-t", id:"127641" },
+      1: { title:"Animation Throwdown", game:"animation-", id:"271381" },
+      2: { title:"BattleHand", game:"battlehand", id:"261051" },
+      3: { title:"Bit Heroes", game:"bit-heroes", id:"266462" },
+      4: { title:"Clicker Heroes", game:"clicker-he", id:"216826" },
+      5: { title:"Cosmos Quest: The Origin", game:"cosmos-que", id:"276144" },
+      6: { title:"Crusaders of the Lost Idols", game:"crusaders-", id:"243503" },
+      7: { title:"Crush Crush", game:"crush-crus", id:"256923" },
+      8: { title:"DPS IDLE", game:"dps-idle", id:"316416" },
+      9: { title:"Dungeon Crusher: Soul Hunters", game:"dungeon-cr", id:"283352" },
+      10: { title:"Firestone Idle RPG", game:"firestone", id:"299519" },
+      11: { title:"Idle Champions of the Forgotten Realms", game:"idle-champ", id:"303551" },
+      12: { title:"Idle Grindia: Dungeon Quest", game:"idle-grind", id:"317742" },
+      13: { title:"Idle Monster TD", game:"idle-monst", id:"315606" },
+      14: { title:"Idling to Rule the Gods", game:"idling-to-become-god", id:"218178" },
+      15: { title:"Incremental Epic Hero", game:"ncrementa", id:"317607" },
+      16: { title:"Iron Rage", game:"iron-rage", id:"296562" },
+      17: { title:"Mighty Party", game:"mighty-par", id:"272366" },
+      18: { title:"Mob Wars: La Cosa Nostra", game:"mob-wars-l", id:"161320" },
+      19: { title:"Mutilate-a-Doll 2", game:"mutilate-a", id:"190329" },
+      20: { title:"Naruto Online", game:"naruto-onl", id:"269125" },
+      21: { title:"NGU IDLE", game:"ngu-idle", id:"287709" },
+      22: { title:"Pincremental", game:"pincremental", id:"321971" },
+      23: { title:"Realm Grinder", game:"realm-grin", id:"239558" },
+      24: { title:"Realm of the Mad God", game:"realm-of-t", id:"127641" },
+      25: { title:"Spellstone", game:"spellstone", id:"248326" },
+      26: { title:"Supermechs", game:"supermechs", id:"171560" },
+      27: { title:"Synergism", game:"synergism", id:"320578" },
+      28: { title:"The Perfect Tower", game:"the-perfec", id:"258553" },
+      29: { title:"Tyrant Unleashed", game:"tyrant-unl", id:"208033" },
+      30: { title:"Wartune", game:"wartune", id:"303599" }
     };
 
     var x;
     for (x in game_list) {
       var li = document.createElement("li");
       li.innerHTML = game_list[x].title;
-      li.setAttribute("style",`
-        cursor: pointer;
-        display: block;
-        text-align: left;
-        padding-left: 8px;
-      `);
       li.setAttribute("title",game_list[x].title);
       li.setAttribute("game",game_list[x].game);
       li.setAttribute("id",game_list[x].id);
@@ -77,3 +104,5 @@ function initialise() {
 }
 
 var initialise = setInterval(initialise, 1000);
+
+//console.log('{ title:"' + document.querySelectorAll('[itemprop="name"]')[0].innerHTML + '"", game:""' + kong_ads._targetingParams.game + '", id:"' + kong_ads._targetingParams.game_id + '" },')
