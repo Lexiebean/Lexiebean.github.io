845bc93c54d832cf283566e0b44de913937fe5e5
BrkIt <BrkIt@users.noreply.github.com>
2020-08-23 22:18:00 +1000

    Update KongChatroomSwitcher.user.js
    Bug fixes

--- KongChatroomSwitcher.user.js
+++ KongChatroomSwitcher.user.js
@@ -4,7 +4,7 @@
 // @match       https://www.kongregate.com/games/*
 // @match       http://www.kongregate.com/games/*
 // @grant       none
-// @version     1.1
+// @version     1.1.1
 // @author      @Alexiea#6630
 // @downloadURL https://github.com/BrkIt/BrkIt.github.io/raw/master/KongChatroomSwitcher.user.js
 // @updateURL   https://github.com/BrkIt/BrkIt.github.io/raw/master/KongChatroomSwitcher.user.js
@@ -110,7 +110,7 @@ function initialise() {
       li.setAttribute("title",game_list[x].title);
       li.setAttribute("game",game_list[x].game);
       li.setAttribute("id",game_list[x].id);
-	    li.onclick = function(e) { savedchat = document.getElementsByClassName("chat_message_window")[1]; active = document.querySelectorAll(".activeChat");for(var i=0;i<active.length;i++) { active[i].className = ""; }; this.className = "activeChat"; holodeck.selectRoom({"name": this.getAttribute("title") + "- Room #01","xmpp_name": this.getAttribute("id") + "-" + this.getAttribute("game") + "-1","type":"game"}); var title = this.getAttribute("title"); setTimeout( function() { document.getElementsByClassName("chat_message_window")[1].innerHTML = savedchat.innerHTML; var div = document.createElement("div"); div.className = "chat-message"; div.innerHTML = "==== Joining " + title + " - Room #01 ===="; div.style.textAlign = "center"; div.style.background = "#666"; div.style.color = "white"; document.getElementsByClassName("chat_message_window")[1].appendChild(div); }, 1000, title); };
+	    li.onclick = function(e) { holodeck.activeDialogue().displayMessage("Joined", this.getAttribute("title") + " - Room #01", { "class": "error" }, { non_user: true }); savedchat = document.getElementsByClassName("chat_message_window")[1]; active = document.querySelectorAll(".activeChat");for(var i=0;i<active.length;i++) { active[i].className = ""; }; this.className = "activeChat"; holodeck.selectRoom({"name": this.getAttribute("title") + "- Room #01","xmpp_name": this.getAttribute("id") + "-" + this.getAttribute("game") + "-1","type":"game"}); setTimeout( function() { document.getElementsByClassName("chat_message_window")[1].innerHTML = savedchat.innerHTML; }, 1000); };    //li.onclick = function(e) { savedchat = document.getElementsByClassName("chat_message_window")[1]; active = document.querySelectorAll(".activeChat");for(var i=0;i<active.length;i++) { active[i].className = ""; }; this.className = "activeChat"; holodeck.selectRoom({"name": this.getAttribute("title") + "- Room #01","xmpp_name": this.getAttribute("id") + "-" + this.getAttribute("game") + "-1","type":"game"}); var title = this.getAttribute("title"); };
 	    ul.appendChild(li);
       
       //Add radio buttons to select default
